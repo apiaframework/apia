f21ea485ab0dd528419a242238ee7ddad42ec696
Adam Cooke <me@adamcooke.io>
2020-04-22 15:24:28 +0000

    update default requires

--- lib/apeye.rb
+++ lib/apeye.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
-require 'apeye/type'
+require 'apeye/api'
 require 'apeye/argument_set'
+require 'apeye/authenticator'
 require 'apeye/enum'
+require 'apeye/error'
+require 'apeye/type'
