2b1edad3f01a92b546974ce00f586463493c693c
Adam Cooke <me@adamcooke.io>
2020-04-24 13:39:12 +0000

    add additional tests for authenticator

--- spec/specs/apeye/authenticator_spec.rb
+++ spec/specs/apeye/authenticator_spec.rb
@@ -12,4 +12,27 @@ describe APeye::Authenticator do
       expect(authenticator.definition.type).to eq :bearer
     end
   end
+
+  context '.potential_errors' do
+    it 'should allow potential errors to be defined' do
+      error = APeye::Error.create do
+        code :some_code
+      end
+
+      authenticator = APeye::Authenticator.create do
+        potential_error error
+      end
+
+      expect(authenticator.definition.potential_errors.first).to eq error
+    end
+  end
+
+  context '.action' do
+    it 'should allow an action to be defined' do
+      authenticator = APeye::Authenticator.create do
+        action { 10 }
+      end
+      expect(authenticator.definition.action.call).to eq 10
+    end
+  end
 end
