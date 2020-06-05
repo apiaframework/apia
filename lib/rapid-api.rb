41656c6050bbf93e219c89759970200b0e01164a
Adam Cooke <me@adamcooke.io>
2020-06-05 13:15:38 +0100

    remove rapid-api because the gem is called just rapid now

--- lib/rapid-api.rb
+++
@@ -1,9 +0,0 @@
-# rubocop:disable Naming/FileName
-
-# frozen_string_literal: true
-
-# Stub so that rapid is automatically required by Bundler.
-
-require 'rapid'
-
-# rubocop:enable Naming/FileName
