4dad680cff0f67cdac05afd3f1dfa5968d9588c1
Adam Cooke <me@adamcooke.io>
2020-07-17 21:57:41 +0100

    support for parsing boolean values from strings & integers

--- lib/rapid/scalars/boolean.rb
+++ lib/rapid/scalars/boolean.rb
@@ -11,12 +11,25 @@ module Rapid
 
       name 'Boolean'
 
+      TRUE_VALUES = [true, 'true', 'yes', 1, '1'].freeze
+      FALSE_VALUES = [false, 'false', 'no', 0, '0'].freeze
+
       cast do |value|
         value ? true : false
       end
 
       validator do |value|
         value.is_a?(TrueClass) || value.is_a?(FalseClass)
+      end
+
+      parse do |value|
+        if TRUE_VALUES.include?(value)
+          true
+        elsif FALSE_VALUES.include?(value)
+          false
+        else
+          raise Rapid::ParseError, 'Boolean must be provided as a boolean, as a string containing true or false or as 0 or 1 as an integer.'
+        end
       end
 
     end
--- spec/specs/rapid/scalars/boolean_spec.rb
+++ spec/specs/rapid/scalars/boolean_spec.rb
@@ -15,12 +15,33 @@ describe Rapid::Scalars::Boolean do
     {
       true => true,
       false => true,
-      'true' => false,
-      1 => false,
+      'llama' => false,
+      123 => false,
       Class.new => false
     }.each do |input, expectation|
       it "should return #{expectation} for #{input.inspect}" do
         expect(described_class.valid?(input)).to be expectation
+      end
+    end
+  end
+
+  context '.parse' do
+    {
+      true => true,
+      false => false,
+      1 => true,
+      'true' => true,
+      '0' => false,
+      'false' => false
+    }.each do |input, expectation|
+      it "should be able to parse integers & strings (#{input.inspect} -> #{expectation.inspect})" do
+        expect(described_class.parse(input)).to eq expectation
+      end
+    end
+
+    ['potato', '1234', Class.new, :something, -1].each do |input|
+      it "should raise a parse error for invalid values (#{input.inspect})" do
+        expect { described_class.parse(input) }.to raise_error Rapid::ParseError
       end
     end
   end
