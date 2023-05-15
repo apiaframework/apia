69d1c11da68f20231cd0ab9d14668608264e2949
Adam Cooke <adam@adam.ac>
2023-05-15 11:19:07 +0100

    chore: add release-please

--- lib/apia/version.rb
+++ lib/apia/version.rb
@@ -2,11 +2,6 @@
 
 module Apia
 
-  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
-  if File.file?(VERSION_FILE_ROOT)
-    VERSION = File.read(VERSION_FILE_ROOT).strip.sub(/\Av/, '')
-  else
-    VERSION = '0.0.0.dev'
-  end
+  VERSION = '3.3.1'
 
 end
